2c0c7d75aa48aa58ffa73f00a29ac5dabab7b6bf
maria-shpatserman <sokunova@gmail.com>
2024-10-16 22:37:04 +0800

    udemy collections 1 lesson

---
+++ src/test/java/ru/netunix/collections/udemy/UdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netunix.collections.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
